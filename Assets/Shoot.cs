9241154cc9234e3c05af88c585b69f894069e679
Andrew Hansen <123025570+HansenSAndrew@users.noreply.github.com>
2023-11-04 16:24:11 -0400

    Worked on Bullet Script and Made Alley (didn't know Kiser already made one)

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -8,10 +8,17 @@ public class Shoot : MonoBehaviour
     public GameObject bullet;
     public GameObject FiringPoint;
     public AudioSource audioFile;
+    private int bullets;
+    private bool canShoot;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Made it where you only have 6 bullets.
+        // However, this means there needs to be some sort
+        // of reload functionality.
+        bullets = 6;
+        canShoot = true;
     }
 
     // Update is called once per frame
@@ -25,18 +32,35 @@ public class Shoot : MonoBehaviour
 
     public void Fire()
     {
-        int bullets = 1;
-        if (Vector3.Distance(GameObject.Find("Right Controller").transform.position, GameObject.Find("Left Controller").transform.position) < .04)
-        {
-            bullets = 3;
-        }
-        for (int i = 0; i < bullets; i++)
+        // Kiser, what is this code for??
+
+        // int bullets = 1;
+        // if (Vector3.Distance(GameObject.Find("Right Controller").transform.position, GameObject.Find("Left Controller").transform.position) < .04)
+        // {
+        //     bullets = 3;
+        // }
+
+        // for (int i = 0; i < bullets; i++)
+
+        if (bullets > 0 && canShoot)
         {
             GameObject newBullet = Instantiate(bullet, FiringPoint.transform.position, FiringPoint.transform.rotation);
             newBullet.SetActive(true);
             newBullet.tag = "Bullet";
             newBullet.GetComponent<Rigidbody>().AddForce(FiringPoint.transform.forward * 1500);
+            audioFile.Play();
+            StartCoroutine(fireDelay(newBullet));
+            bullets--;
         }
-        audioFile.Play();
+    }
+
+    // Function to deal with trigger spam.
+    IEnumerator fireDelay(GameObject bullet)
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(0.75f);
+        // Fixed infinite bullets being made.
+        Destroy(bullet);
+        canShoot = true;
     }
 }
