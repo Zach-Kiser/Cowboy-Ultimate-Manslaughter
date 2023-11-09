02f3cf210e436a981319c5ade77d99d244fe7c03
lukahine <hinelp@gmail.com>
2023-11-09 10:33:55 -0500

    Fires with Trigger, Deleted Main Menu, fixed accuracy in Alley

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
 
 public class Shoot : MonoBehaviour
 {
+    public InputActionReference triggerInputActionReference;
     public GameObject bullet;
     public GameObject FiringPoint;
     public AudioSource audioFile;
@@ -24,11 +28,13 @@ public class Shoot : MonoBehaviour
     void Update()
     {
 
+        float triggerValue = triggerInputActionReference.action.ReadValue<float>();
+
         // TODO: Fix bug with Fan the Hammer. Currently, in the Alley Scene, I gave the player 100 bullets.
         // However, with our current implementation of Fan the Hammer, that just shoots out 100 bullets no
         // matter what. We need some kind of system to check if the player has left the original "Fan the
         // Hammer" distance AND if they have stop holding the trigger (fire1).
-        if (Input.GetButtonDown("Fire1") && bullets > 0 && canShoot)
+        if (triggerValue > 0.2 && bullets > 0 && canShoot)
         {
             if ((Vector3.Distance(leftController.transform.position, transform.position) < 0.2) && Input.GetButton("Fire1"))
             {
@@ -42,7 +48,8 @@ public class Shoot : MonoBehaviour
         }
 
         //Temp Reload Function
-        if (Input.GetButtonDown("Fire2")) {
+        if (Input.GetButtonDown("Fire2"))
+        {
             bullets = 6;
         }
     }
@@ -51,24 +58,24 @@ public class Shoot : MonoBehaviour
     {
         // while (Input.GetButton("Fire1") && amount > 0)
         // {
-            canShoot = false;
+        canShoot = false;
 
-            GameObject newBullet = Instantiate(bullet, FiringPoint.transform.position, FiringPoint.transform.rotation);
-            newBullet.SetActive(true);
-            newBullet.tag = "Bullet";
-            newBullet.GetComponent<Rigidbody>().AddForce(FiringPoint.transform.forward * 1500);
+        GameObject newBullet = Instantiate(bullet, FiringPoint.transform.position, FiringPoint.transform.rotation);
+        newBullet.SetActive(true);
+        newBullet.tag = "Bullet";
+        newBullet.GetComponent<Rigidbody>().AddForce(FiringPoint.transform.forward * 1500);
 
-            audioFile.Play();
-            bullets--;
+        audioFile.Play();
+        bullets--;
 
-            muzzleFlash.SetActive(true);
-            yield return new WaitForSeconds(0.15f);
-            muzzleFlash.SetActive(false);
+        muzzleFlash.SetActive(true);
+        yield return new WaitForSeconds(0.15f);
+        muzzleFlash.SetActive(false);
 
-            
-            yield return new WaitForSeconds(0.5f);
-            Destroy(newBullet);
-            canShoot = true;
+
+        yield return new WaitForSeconds(0.5f);
+        Destroy(newBullet);
+        canShoot = true;
         // }
     }
 }
