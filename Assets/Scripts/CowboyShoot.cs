e748095d49b20860c32416497a508fb4d3edc045
Zach Kiser <kiserzt@dukes.jmu.edu>
2023-11-21 21:00:39 -0500

    Add cylinder rotation and enemy now shoots back

--- Assets/Scripts/CowboyShoot.cs
+++ Assets/Scripts/CowboyShoot.cs
@@ -1,22 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Simulation;
+using static UnityEngine.Random;
 
-public class Shoot : MonoBehaviour
+public class CowBoyShoot : MonoBehaviour
 {
-    public InputActionReference triggerInputActionReference;
+    //public InputActionReference triggerInputActionReference;
     public GameObject bullet;
     public GameObject FiringPoint;
     public AudioSource audioFile;
-    public GameObject leftController;
-    public GameObject muzzleFlash;
-    public GameObject cylinder;
-    public GameObject clinder2;
+    //public GameObject leftController;
+    //public GameObject muzzleFlash;
+    //public GameObject cylinder;
+    //public GameObject clinder2;
 
     public int bullets;
     private bool canShoot;
@@ -31,29 +31,15 @@ public class Shoot : MonoBehaviour
     void Update()
     {
 
-        float triggerValue = triggerInputActionReference.action.ReadValue<float>();
+        //float triggerValue = triggerInputActionReference.action.ReadValue<float>();
 
         // TODO: Fix bug with Fan the Hammer. Currently, in the Alley Scene, I gave the player 100 bullets.
         // However, with our current implementation of Fan the Hammer, that just shoots out 100 bullets no
         // matter what. We need some kind of system to check if the player has left the original "Fan the
         // Hammer" distance AND if they have stop holding the trigger (fire1).
-        if (triggerValue > 0.2 && bullets > 0 && canShoot)
+        if (canShoot && Random.value < .01)
         {
-            if ((Vector3.Distance(leftController.transform.position, transform.position) < 0.2) && Input.GetButton("Fire1"))
-            {
-                StartCoroutine(Fire(bullets));
-            }
-
-            else
-            {
-                StartCoroutine(Fire(1));
-            }
-        }
-
-        //Temp Reload Function
-        if (Input.GetButtonDown("Fire2"))
-        {
-            bullets = 6;
+            StartCoroutine(Fire(1));
         }
     }
 
@@ -65,8 +51,10 @@ public class Shoot : MonoBehaviour
 
         GameObject newBullet = Instantiate(bullet, FiringPoint.transform.position, FiringPoint.transform.rotation);
         newBullet.SetActive(true);
-        newBullet.tag = "Bullet";
-        newBullet.GetComponent<Rigidbody>().AddForce(FiringPoint.transform.forward * 1500);
+        // make the bullets massive so you can see them
+        newBullet.transform.localScale += new Vector3(3f, 3f, 3f);
+        newBullet.tag = "EnemyBullet";
+        newBullet.GetComponent<Rigidbody>().AddForce(FiringPoint.transform.forward * 1000);
 
         audioFile.Play();
         bullets--;
@@ -75,7 +63,7 @@ public class Shoot : MonoBehaviour
 
         // Couldn't get rotation to work, so my current solution is to just
         // show and hide a slightly rotated cylinder
-        if (cylinder.activeSelf)
+        /*if (cylinder.activeSelf)
         {
             cylinder.SetActive(false);
             clinder2.SetActive(true);
@@ -84,16 +72,18 @@ public class Shoot : MonoBehaviour
         {
             cylinder.SetActive(true);
             clinder2.SetActive(false);
-        }
+        }*/
 
-        muzzleFlash.SetActive(true);
+        /*muzzleFlash.SetActive(true);
         yield return new WaitForSeconds(0.15f);
         muzzleFlash.SetActive(false);
+        */
 
 
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(Random.value * 10);
         Destroy(newBullet);
         canShoot = true;
         // }
     }
 }
+
