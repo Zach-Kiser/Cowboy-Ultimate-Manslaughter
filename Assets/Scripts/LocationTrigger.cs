d8aa538d0b6a235a75a221a70c54df2fb13489e9
lukahine <hinelp@gmail.com>
2023-12-09 12:26:48 -0500

    Cowboy Ragdolls
    please work

--- Assets/Scripts/LocationTrigger.cs
+++ Assets/Scripts/LocationTrigger.cs
@@ -4,36 +4,65 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Unity.XR.CoreUtils;
 
 
 public class LocationTrigger : MonoBehaviour
 {
-    public GameObject opponent; 
+    public GameObject opponent;
     public TextMeshProUGUI score;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Bullet"))
         {
-            string oldScore = score.text.Split("Score: ")[1];
-            if (oldScore == "500")
+            // string oldScore = score.text.Split("Score: ")[1];
+            // if (oldScore == "500")
+            // {
+            //     SceneManager.LoadScene("Alley");
+            // }
+            // score.text = "Score: " + (int.Parse(oldScore) + 100);
+            List<GameObject> allChildren = GetAllChildren(opponent);
+            foreach (GameObject child in allChildren)
             {
-                SceneManager.LoadScene("Alley");
+                Rigidbody childRigidbody = child.GetComponent<Rigidbody>();
+                if (childRigidbody != null)
+                {
+                    // Activate gravity for the GameObject with Rigidbody
+                    childRigidbody.useGravity = true;
+                }
             }
-            score.text = "Score: " + (int.Parse(oldScore) + 100);
-            opponent.SetActive(false);
+
         }
     }
+
+    List<GameObject> GetAllChildren(GameObject parent)
+    {
+        List<GameObject> childList = new List<GameObject>();
+
+        foreach (Transform child in parent.transform)
+        {
+            // Add the immediate child
+            childList.Add(child.gameObject);
+
+            // Recursively add all children of the child
+            List<GameObject> childrenOfChild = GetAllChildren(child.gameObject);
+            childList.AddRange(childrenOfChild);
+        }
+
+        return childList;
+    }
+
 }
