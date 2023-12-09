18c52fb978360697f06e431da39ec8c05b00555b
Jimi McDermott <mcder3jm@dukes.jmu.edu>
2023-12-09 11:37:02 -0500

    Main Menu and Practice Scene

---
+++ Assets/Scripts/practicescene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class practicescene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            other.GetComponent<TrailRenderer>().enabled = false;
+            StartCoroutine(startup());
+        }
+    }
+
+    IEnumerator startup() {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("PracticeScene");
+    }
+
+    
+}
