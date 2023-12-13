48e5bdb506483e0e007c5f4acabf21d113582ab3
Zach Kiser <kiserzt@dukes.jmu.edu>
2023-12-13 14:17:10 -0500

    fix it so you win if you shoot all the cowboys

--- Assets/Scripts/LocationTrigger.cs
+++ Assets/Scripts/LocationTrigger.cs
@@ -29,7 +29,7 @@ public class LocationTrigger : MonoBehaviour
         if (other.CompareTag("Bullet"))
         {
             string oldScore = score.text.Split("Score: ")[1];
-            if (oldScore == "400")
+            if (oldScore == "300")
             {
                 SceneManager.LoadScene("Alley");
             }
